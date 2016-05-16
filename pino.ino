fa67827f99315235709506a553e2aa5875d086c9
cheng <gnuchex@gmail.com>
2016-05-16 06:34:39 -0700

    modified color jump, and cleaned up code

--- pino.ino
+++ pino.ino
@@ -1,26 +1,19 @@
-//Please use the "Adafruit Trinket 8MHz" board setting and 
-//select "USBTinyISP" programmer on your Arduino IDE 
+//Please use the "Adafruit Trinket 8MHz" board setting and
+//select "USBTinyISP" programmer on your Arduino IDE
 //for Trinket to use 8pino.
+//unplug and plug back in before uploading 
 
-
+//reported glitch on linux with usb3
 
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR_ATtiny85__ // Trinket, Gemma, etc.
 #include <avr/power.h>
 #endif
 
-#define PIN 1
-
-
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(59, PIN);
-
-uint8_t  mode   = 0, // Current animation effect
-         offset = 0; // Position of spinny eyes
-//uint32_t color  = 0xFF0000; // Start red
-uint32_t prevTime;
-
-//bounce switch
-const int switchPin = 2 ;
+#define NEOP_PIN 1
+#define SWITCH_PIN 2  //bounce switch
+#define PIXEL_CNT 60 //60//59 // 30 
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(PIXEL_CNT, NEOP_PIN);
 
 //COLOR
 float rainbowStep = 0;
@@ -31,37 +24,33 @@ void setup() {
 #endif
   pixels.begin();
   pixels.setBrightness(85); // 1/3 brightness
-//  prevTime = millis();
 
- pinMode(switchPin, INPUT);
+  pinMode(SWITCH_PIN, INPUT);
+
+  //startup animation
+//   colorTravel(pixels.Color(240, 0, 240), 30); // purple
+  colorWipe(pixels.Color(240, 0, 240), 15); // purple
+  colorWipe(pixels.Color(0, 0, 0), 15); // dark 
+
 }
 
 void loop() {
-  uint8_t  i;
-//  uint32_t t;
 
-  int buttonState = digitalRead(switchPin);
+
+  int buttonState = digitalRead(SWITCH_PIN);
   if (buttonState == HIGH) {
-    //color >>= 8;                 // Next color R->G->B
-    rainbowStep += 5; 
+    rainbowStep += 30;  //jump 
 
-    //if (!color) color = 0xFF0000; // Reset to red
   } else {
-    rainbowStep += 0.05;  //the integer increament every 5 cycles..
-    
+    rainbowStep += 0.05; //small increment
   }
-  
-    if (rainbowStep >255 ){
-      rainbowStep = 0 ;
-    }
 
-    rainbowByStep(rainbowStep);
-    
-   //for (int i = 0; i <59; i++) {
-   //   pixels.setPixelColor(i, color);
-   //}
-   
-   pixels.show();
+  if (rainbowStep > 255 ) {
+    rainbowStep = (int(rainbowStep))%255 ;
+  }
+
+  rainbowByStep(rainbowStep);
+
 }
 
 void rainbow(uint8_t wait) {
@@ -76,13 +65,34 @@ void rainbow(uint8_t wait) {
   }
 }
 
-void rainbowByStep(float rStep){
+void rainbowByStep(float rStep) {
 
-  int j = (int)rStep %256;
+  int j = (int)rStep % 256;
   for (int i = 0; i < pixels.numPixels(); i++) {
     pixels.setPixelColor(i, Wheel((i + j) & 255));
   }
   pixels.show();
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for (uint16_t i = 0; i < pixels.numPixels(); i++) {
+    pixels.setPixelColor(i, c);
+    pixels.show();
+    delay(wait);
+    
+  }
+}
+
+// Fill the dots one after the other with a color
+void colorTravel(uint32_t c, uint8_t wait) {
+  for (uint16_t i = 0; i < pixels.numPixels(); i++) {
+    pixels.setPixelColor(i, c);
+    pixels.show();
+    delay(wait);
+    pixels.setPixelColor(i, pixels.Color(0,0,0));
+    pixels.show();
+  }
 }
 
 
@@ -92,11 +102,11 @@ uint32_t Wheel(byte WheelPos) {
   WheelPos = 255 - WheelPos;
   if (WheelPos < 85) {
     return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } 
+  }
   else if (WheelPos < 170) {
     WheelPos -= 85;
     return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  } 
+  }
   else {
     WheelPos -= 170;
     return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
